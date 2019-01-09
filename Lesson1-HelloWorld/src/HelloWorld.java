6858869a034ba2566bc15e736969c360f5ba3e67
Marloun Bayari <mbayari@gmail.com>
2019-01-09 17:31:32 -0600

    Adding Lesson1 as per repository refresh

---
+++ Lesson1-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hola="Hello World!";
+		System.out.println("Hello world!");
+		System.out.println(hola);
+
+	}
+
+}
