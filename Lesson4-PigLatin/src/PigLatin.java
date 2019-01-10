8957e58213f3e426d653495a7386f38e505a1ea4
Marloun Bayari <mbayari@gmail.com>
2019-01-09 18:12:51 -0600

    update Pig Latin project

--- Lesson4-PigLatin/src/PigLatin.java
+++ Lesson4-PigLatin/src/PigLatin.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class PigLatin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Let's convert a word to Pig Latin...");
+		System.out.println("Enter a word to convert.");
+		
+		Scanner reader = new Scanner(System.in);
+		String WordToConvert = reader.nextLine();
 
 	}
 
