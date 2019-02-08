70016f355cc91a1348e4323dfab56a6d48a257d9
Marloun Bayari <mbayari@gmail.com>
2019-02-08 16:51:42 -0600

    Uploading TimeSheetApp for class Assignemnt Feb 8, 2019

---
+++ TimeSheetApp/src/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Marloun_Bayari
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Controller menu = new Controller();		
+		boolean quit = false;
+		
+		while(!(quit)) {
+			menu.start();
+		}
+	}
+
+}
